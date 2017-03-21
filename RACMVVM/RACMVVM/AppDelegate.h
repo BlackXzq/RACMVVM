9a4ad212a8cfa92a2a8b6fbf1e0ca7b2804f83e9
xuzhanqiang <xuzhanqiang@hujiang.com>
2017-03-21 14:15:58 +0800

    add 工程

---
+++ RACMVVM/RACMVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACMVVM
+//
+//  Created by nero on 17/3/21.
+//  Copyright © 2017年 xuzhanqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
